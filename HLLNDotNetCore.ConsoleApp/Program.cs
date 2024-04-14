2a3d695aea8fa1174eb3c51567bfb9b90c9c2a2b
Htet Lin Lin Naing <htetlinlinnaing.myanmar@gmail.com>
2024-04-14 19:02:17 +0630

    CRUD

--- HLLNDotNetCore.ConsoleApp/Program.cs
+++ HLLNDotNetCore.ConsoleApp/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using AMMDotNetCore.ConsoleApp;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
 Console.WriteLine("Hello, World!");
+
+/* SqlConnectionStringBuilder stringBuilder = new SqlConnectionStringBuilder();
+stringBuilder.DataSource = "USER";
+stringBuilder.InitialCatalog = "HLLNDotNetCore";
+stringBuilder.UserID = "sa";
+stringBuilder.Password = "sa123";
+
+SqlConnection connection = new SqlConnection(stringBuilder.ConnectionString);
+connection.Open();
+Console.WriteLine("Connection Open.");
+
+string query = "select * from Tbl_Blog";
+ SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter SqlDataAdapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+SqlDataAdapter.Fill(dt); 
+
+connection.Close();
+Console.WriteLine("Connection Close");
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine("Blog Id =>"+dr["BlogId"]);
+    Console.WriteLine("Blog Title =>" + dr["BlogTitle"]);
+    Console.WriteLine("Blog Author =>" + dr["BlogAuthor"]);
+    Console.WriteLine("Blog Content =>" + dr["BlogContent"]);
+    Console.WriteLine("-------------------------------");
+} */
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create("title", "author", "content");
+//adoDotNetExample.Update(11, "test title", "test author", "test content");
+//adoDotNetExample.Delete(11);
+adoDotNetExample.Edit(11);
+adoDotNetExample.Edit(1);
+
+Console.ReadKey();
