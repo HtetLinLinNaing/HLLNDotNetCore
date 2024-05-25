cfa50015730c17033c099e7033c1d6dc3247ad9c
Htet Lin Lin Naing <htetlinlinnaing.myanmar@gmail.com>
2024-05-25 11:43:33 +0630

    [COMPLETED] Pizza API

---
+++ HLLNDotNetCore.PizzaApi/ConnectionStrings.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Data.SqlClient;
+
+namespace HLLNDotNetCore.PizzaApi;
+
+internal static class ConnectionStrings {
+    public static SqlConnectionStringBuilder SqlConnectionStringBuilder = new SqlConnectionStringBuilder() {
+        DataSource = "GW",
+        InitialCatalog = "HLLNDotNetCore",
+        IntegratedSecurity = true,
+        Encrypt = false,
+        TrustServerCertificate = true
+    };
+}
